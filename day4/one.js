5f254d62ddadb9efc5e9915881105cf9aa31e187
Justin Silver <jsilver@doublesharp.com>
2020-12-04 09:43:45 -0800

    updates

--- day4/one.js
+++ day4/one.js
@@ -1,10 +1,11 @@
 
 const list = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-const base = list.reduce((s, k) => { s[k] = false; return s; }, {});
+const base = list.reduce((keys, k) => ({ ...keys, [k]: false }), {});
 const valid = require("fs").readFileSync("./input.txt", "utf8").split("\n\n")
   .reduce((valid, line) => valid + (Object.values(
-    line.replace(/\n/g, " ").split(" ").reduce((s, p) => {
-      const [k] = p.split(":"); s[k] = true; return s;
+    line.replace(/\n/g, " ").split(" ").reduce((keys, pair) => {
+      const [k] = pair.split(":"); 
+      return { ...keys, [k]: true };
     }, { ...base })
   ).every((value) => value) ? 1 : 0), 0);
 console.log(`There are ${valid} valid passports.`);
--- day4/two.js
+++ day4/two.js
@@ -1,5 +1,5 @@
 const list = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-const base = list.reduce((keys, k) => { keys[k] = false; return keys; }, {});
+const base = list.reduce((keys, k) => ({ ...keys, [k]: false }), {});
 const validators = {
   byr: (x) => x >= 1920 && x <= 2002,
   iyr: (x) => x >= 2010 && x <= 2020,
@@ -17,8 +17,7 @@ const valid = require("fs").readFileSync("./input.txt", "utf8").split("\n\n")
   .reduce((c, line) => c + (Object.values(line.replace(/\n/g, " ").split(" ")
     .reduce((keys, pair) => {
       const [k, v] = pair.split(":");
-      validators[k] && (keys[k] = validators[k](v));
-      return keys;
+      return !validators[k] ? keys : { ...keys, [k]: validators[k](v) };
     }, { ...base })
   ).every((value) => value) ? 1 : 0), 0);
 console.log(`There are ${valid} valid passports.`);
