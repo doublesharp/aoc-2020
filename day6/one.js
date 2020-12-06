a5b69f1fadf605830c7726801b426e202ea2dd95
Justin Silver <jsilver@doublesharp.com>
2020-12-06 00:09:25 -0800

    day 6

---
+++ day6/one.js
@@ -0,0 +1,6 @@
+const sum = require("fs").readFileSync("./input.txt", "utf8").split("\n\n")
+  .reduce((sum, group) => sum + Object.keys(
+    group.split("\n").reduce((letters, person) => person.split("")
+      .reduce((letters, letter) => ({ ...letters, [letter]: true }), letters)
+  , {})).length, 0);
+console.log(`The sum of the counts is ${sum}.`);
